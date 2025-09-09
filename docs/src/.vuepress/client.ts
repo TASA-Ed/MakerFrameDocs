701b71ac406d7febc1c6c2c2a85221035c86c23a
ccd2s <tasaedem@outlook.com>
2025-09-09 19:15:33 +0800

    remove: 一些没用的东西

--- docs/src/.vuepress/client.ts
+++ docs/src/.vuepress/client.ts
@@ -1,12 +1,8 @@
 import qqGroupLink from "./components/qqGroupLink.js";
 import { defineClientConfig } from "vuepress/client";
-import publicNotFound from "./layouts/publicNotFound.vue";
 
 export default defineClientConfig({
   enhance: ({ app }) => {
     app.component("qqGroupLink", qqGroupLink);
-  },
-  layouts: {
-    NotFound: publicNotFound,
   },
 });
